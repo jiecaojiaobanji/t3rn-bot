fc315924231d785a20b4bccc1548aa70a5997732
jiecaojiaobanji <71306961+jiecaojiaobanji@users.noreply.github.com>
2025-01-16 20:02:41 +0200

    Update network_config.py

--- network_config.py
+++ network_config.py
@@ -1,15 +1,15 @@
 # network_config.py
 
 networks = {
-    'Arbitrum': {
-        'rpc_url': 'https://sepolia.arbiscan.io', 
-        'chain_id': 421614, 
+    'Base': {
+        'rpc_url': 'https://sepolia.base.org', 
+        'chain_id': 84532, 
         'contract_address': 
-        '0x8D86c3573928CE125f9b2df59918c383aa2B514D' 
+        '0x30A0155082629940d4bd9Cd41D6EF90876a0F1b5' 
     },
-    'Blast  Sepolia': {
-        'rpc_url': 'https://sepolia.blastscan.io',
-        'chain_id': 168587773,
-        'contract_address': '0x1D5FD4ed9bDdCCF5A74718B556E9d15743cB26A2'
+    'OP Sepolia': {
+        'rpc_url': 'https://endpoints.omniatech.io/v1/op/sepolia/public',
+        'chain_id': 11155420,
+        'contract_address': '0xF221750e52aA080835d2957F2Eed0d5d7dDD8C38'
     },
 }
