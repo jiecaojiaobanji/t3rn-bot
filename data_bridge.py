a43ab0cf7e22604c1af4e37550026a2036b1379a
jiecaojiaobanji <71306961+jiecaojiaobanji@users.noreply.github.com>
2025-01-17 01:58:01 +0800

    Add files via upload

---
+++ data_bridge.py
@@ -0,0 +1,9 @@
+# data_bridge.py
+
+data_bridge = {
+    # Data bridge Arbitrum
+    "Arbitrum - Blast Sepolia": '',
+
+    # Data bridge Blast Sepolia
+    "Blast - Arbitrum": '',
+}
---
+++ network_config.py
@@ -0,0 +1,15 @@
+# network_config.py
+
+networks = {
+    'Arbitrum': {
+        'rpc_url': 'https://sepolia.arbiscan.io', 
+        'chain_id': 421614, 
+        'contract_address': 
+        '0x8D86c3573928CE125f9b2df59918c383aa2B514D' 
+    },
+    'Blast  Sepolia': {
+        'rpc_url': 'https://sepolia.blastscan.io',
+        'chain_id': 168587773,
+        'contract_address': '0x1D5FD4ed9bDdCCF5A74718B556E9d15743cB26A2'
+    },
+}
