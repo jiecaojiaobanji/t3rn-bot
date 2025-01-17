ef8880746dce815cfe18944e4564253a62da236f
jiecaojiaobanji <71306961+jiecaojiaobanji@users.noreply.github.com>
2025-01-17 07:41:28 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -199,7 +199,7 @@ def main():
         balance = check_balance(web3, my_address)
 
         # 如果余额不足 0.1 ETH，切换到另一个链
-        if balance < 0.1:
+        if balance < 1:
             print(f"{chain_symbols[current_network]}{current_network}余额不足 0.1 ETH，切换到 {alternate_network}{reset_color}")
             current_network, alternate_network = alternate_network, current_network  # 交换链
 
