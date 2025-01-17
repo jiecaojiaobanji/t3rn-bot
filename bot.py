9d7d84eb702ea73784bc2331445c634f14fe6bc5
jiecaojiaobanji <71306961+jiecaojiaobanji@users.noreply.github.com>
2025-01-17 07:44:38 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -58,7 +58,10 @@ def check_balance(web3, my_address):
 # 创建和发送交易的函数
 def send_bridge_transaction(web3, account, my_address, data, network_name):
     nonce = web3.eth.get_transaction_count(my_address, 'pending')
-    value_in_ether = 0.1
+    if network_name == 'Base' :
+            value_in_ether = 1
+        else 
+            value_in_ether = 0.2
     value_in_wei = web3.to_wei(value_in_ether, 'ether')
 
     try:
