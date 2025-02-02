84fc697f0316284d83d27cc1cf92732483e11ba8
jiecaojiaobanji <71306961+jiecaojiaobanji@users.noreply.github.com>
2025-02-02 10:43:33 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -180,8 +180,8 @@ def main():
     print("\n\n")
 
     successful_txs = 0
-    current_network = 'Base'  # 默认从 Base 链开始
-    alternate_network = 'OP Sepolia'
+    current_network = 'OP Sepolia'  # 默认从 Base 链开始
+    alternate_network = 'Base'
 
     while True:
         # 检查当前网络余额是否足够
