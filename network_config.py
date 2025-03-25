241cdd9b81e91f9bfc66b864bb695fe31eebc1b8
jiecaojiaobanji <71306961+jiecaojiaobanji@users.noreply.github.com>
2025-03-26 00:50:21 +0800

    Update network_config.py

--- network_config.py
+++ network_config.py
@@ -2,14 +2,14 @@
 
 networks = {
     'Base': {
-        'rpc_url': 'https://base-sepolia.g.alchemy.com/v2/Xzjbi3vjyMko1hApZ4ctPWJUFAPoxK1K', 
+        'rpc_url': 'https://sepolia.base.org', 
         'chain_id': 84532, 
         'contract_address': 
-        '0x30A0155082629940d4bd9Cd41D6EF90876a0F1b5' 
+        '0xCEE0372632a37Ba4d0499D1E2116eCff3A17d3C3' 
     },
     'OP Sepolia': {
-        'rpc_url': 'https://opt-sepolia.g.alchemy.com/v2/Xzjbi3vjyMko1hApZ4ctPWJUFAPoxK1K',
+        'rpc_url': 'https://sepolia.optimism.io',
         'chain_id': 11155420,
-        'contract_address': '0xF221750e52aA080835d2957F2Eed0d5d7dDD8C38'
+        'contract_address': '0xb6Def636914Ae60173d9007E732684a9eEDEF26E'
     },
 }
